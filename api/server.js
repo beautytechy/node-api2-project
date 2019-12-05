21318cc9daa27acac6ab7aef480f48ca0e109eb3
Nicole Hollis <nicole@emmabeauty.co>
2019-12-04 16:04:33 -0800

    Debugged error messages

--- api/server.js
+++ api/server.js
@@ -8,10 +8,10 @@ server.get('/', (req, res) => {
 });
 
 server.get("/api/posts", (req, res) => {
-    db.find(req.query)
-        .then(post => {
-            console.log(post);
-            res.status(200).json(post);
+    db.find()
+        .then(posts => {
+            console.log(posts);
+            res.status(200).json(posts);
         })
         .catch(err => {
             res
@@ -24,7 +24,7 @@ server.get("/api/posts/:id", (req, res) => {
     const id = req.params.id;
     db.findById(id)
         .then(post => {
-            if (post) {
+            if (post && post.length) {
                 res.status(200).json(post)
             } else {
                 res.status(404)
@@ -42,9 +42,9 @@ server.get("/api/posts/:id", (req, res) => {
 server.get("/api/posts/:id/comments", (req, res) => {
     const id = req.params.id;
     db.findCommentById(id)
-        .then(posts => {
-            if (posts) {
-                res.status(200).json(posts)
+        .then(comments => {
+            if (comments && comments.length) {
+                res.status(200).json(comments)
             } else {
                 res.status(404)
                     .json({ message: "The post with the specified ID does not exist." })
@@ -74,6 +74,30 @@ server.delete("/api/posts/:id/", (req, res) => {
             res
                 .status(500)
                 .json({ error: "The post could not be removed." });
+        });
+});
+
+server.put("/api/posts/:id/", (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    db.update(id, changes)
+        .then(changes => {
+            if (changes) {
+                res.status(200).json(changes);
+            } else if(id) {
+                res
+                    .status(404)
+                    .json({ message: "The post with the specified ID does not exist.." })
+            } else {
+                res.status(400)
+                    .json({ errorMessage: "Please provide title and contents for the post." })
+            }
+        })
+        .catch(error => {
+            console.log("error from PUT/:id", error);
+            res
+                .status(500)
+                .json({ error: "The post information could not be modified." });
         });
 });
 module.exports = server; 
---
+++ data/dbRouter.js
@@ -0,0 +1,7 @@
+
+const db = require("./db");
+
+const router = express.Router();
+
+router.use(express.json());
+
