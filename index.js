3d8ad7efba7cbbd2cbb52fee140be305637b050b
Nicole Hollis <nicole@emmabeauty.co>
2019-12-05 21:54:58 -0800

    Added start script and dynamic port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const server = require("./api/server.js");
 
-server.listen(4001, () => {
-  console.log('\n*** Server Running on http://localhost:4001 ***\n');
+const port = process.env.PORT || 4001;
+
+server.listen(port, () => {
+  console.log('\n*** Server Running on http://localhost:${port} ***\n');
 });
